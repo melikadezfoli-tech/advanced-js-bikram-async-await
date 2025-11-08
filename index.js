a8557e183aa2a4f79cb7dd56a89848a9a1fc8505
Melikadezfoli-Tech <melikadezfoli@gmail.com>
2025-11-08 08:01:07 +0100

    primer ejercicio

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 //DESARROLLA AQUI TUS SOLUCIONES
 
-async function getImageAndName (pokemon){
+//Ejercicio 1 Obtener Pokemon aleatorio:
 
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    let data = await response.json();
-    let name = data.name;
-    let img = data.sprites.front_default;
-    return {name, img}
-    
+//función para obtener el total de Pokemon de forma automática
+const MAX_VALID_ID = 1025; //ultimo pokemon registrado
+async function getTotalPokemonCount() {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1`);
+  const data = await response.json();
+  return Math.min(data.count, MAX_VALID_ID);// automatizamos valor total de Pokemon evitando ids no válidos
 }
+//función para obtener el Pokemon aleatorio
+async function getRandomPokemon(){
+ try{
+    const maxPokemon=await getTotalPokemonCount();
+    const randomId=Math.floor(Math.random()*maxPokemon)+1;
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+    
+    if (!response.ok){
+        console.log (`No se ha podido obtener el Pokemon: ${response.status}`);
+        return null;
+    }
+    const data = await response.json();//sólo se ejecuta si la respuesta es válida
+    
+    return {
+        id: data.id,
+        name: data.name,
+        sprite: data.sprites.front_default ?? "Sin imagen disponible",
+    };
+}catch(error){
+    console.log("No se han podido obtener los datos:", error.message);
+    return null;
+}
+}
+
+getRandomPokemon().then(pokemon=>{
+    if (pokemon){
+        console.log (`Pokemon:${pokemon.name} (Id:${pokemon.id})`)
+        console.log(`Imagen: ${pokemon.sprite}`);
+  } else {
+    console.log("No se pudo obtener un Pokémon.");
+  }
+});
+
+
+
+
